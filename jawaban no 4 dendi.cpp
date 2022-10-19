9c12b707433b55be026721e405e54654b009a042
Dendi9 <113244673+Dendi9@users.noreply.github.com>
2022-10-19 17:09:10 +0700

    Add files via upload

---
+++ jawaban no 4 dendi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a =3; int b = 2, c =1, bil;
+
+  cout << " Bil-A   | B - B| Bil - C\n";
+  cout <<"===========================";
+  for(Bil = 1;bil <= 10; ++bil){
+    a+=b; b+=c; c+=2;
+    cout <<"\n" << a <<"\t|"<< b <<"\t |"<< c;
+    if (c == 13)
+      break;
+    }
+  return o;
+  }
