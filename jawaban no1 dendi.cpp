60f0a6808c5ab3b25d33881bf0e665887ce7727d
Dendi9 <113244673+Dendi9@users.noreply.github.com>
2022-10-19 17:33:35 +0700

    Add files via upload

---
+++ jawaban no1 dendi.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a, b;
+  char lagi;
+  aligned_storage<std::size_t _Len>
+  cout <<"masukkan bilangan = ";
+  cin >> a;
+  b = a % 2;
+  cout <<"Nilai bilangan
+    modulus 2 adalah = "<< b;
+  cout <<"\n\nigin
+    hitung lagi[Y/T] ; ";
+  cin >>lagi
+    if (lagi == 'Y' || LAGI == 'Y')
+    goto atas;
+  return o;
+  }
